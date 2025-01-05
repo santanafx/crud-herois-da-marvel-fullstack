42e7b84a27a891fecb0c7bd13d6159dd4e8f0687
lucas <santanafx@hotmail.com>
2025-01-05 12:53:31 -0300

    feat: created HeroItem component

---
+++ src/Components/Organisms/HeroItem/HeroItem.tsx
@@ -0,0 +1,53 @@
+import { useState } from "react";
+import Button from "../../Atoms/Button/Button";
+import { DeleteForm } from "../../Molecules/DeleteForm/DeleteForm";
+import { HeroForm } from "../../Molecules/HeroForm/HeroForm";
+import { Modal } from "../../Molecules/Modal/Modal";
+import { HeroItemProps } from "./HeroItem.interface";
+import styles from "./HeroItem.module.css";
+
+export const HeroItem = ({ name, abilities, origin, image }: HeroItemProps) => {
+  const [isEditModalOpen, setIsEditModalOpen] = useState(false);
+  const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
+
+  return (
+    <>
+      <div className={styles.heroItemContainer}>
+        <h1>{name}</h1>
+        <h2>Abilities: {abilities}</h2>
+        <h2>Origin: {origin}</h2>
+        <div className={styles.imageContainer}>
+          <img src={image} alt={name} />
+        </div>
+        <div className={styles.buttonsContainer}>
+          <Button
+            size="100px"
+            variant="secondary"
+            onClick={() => setIsEditModalOpen(true)}
+          >
+            Editar
+          </Button>
+          <Button size="100px" onClick={() => setIsDeleteModalOpen(true)}>
+            Deletar
+          </Button>
+        </div>
+      </div>
+      {isEditModalOpen && (
+        <Modal
+          onClose={() => setIsEditModalOpen(false)}
+          isOpen={isEditModalOpen}
+        >
+          <HeroForm editData={{ name, abilities, origin, image }} />
+        </Modal>
+      )}
+      {isDeleteModalOpen && (
+        <Modal
+          onClose={() => setIsDeleteModalOpen(false)}
+          isOpen={isDeleteModalOpen}
+        >
+          <DeleteForm onClose={() => setIsDeleteModalOpen(false)} />
+        </Modal>
+      )}
+    </>
+  );
+};
