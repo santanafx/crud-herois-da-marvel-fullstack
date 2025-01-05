628e0527d1c422d6ddb88f7b4f4af4bb4477520a
lucas <santanafx@hotmail.com>
2025-01-05 01:49:41 -0300

    refactor: added template

--- src/Pages/Homepage/Homepage.tsx
+++ src/Pages/Homepage/Homepage.tsx
@@ -1,29 +1,47 @@
+import { Header } from "../../Components/Molecules/Header/Header";
 import { HeroList } from "../../Components/Molecules/HeroList/HeroList";
+import { HomepageTemplate } from "../../Components/Template/HomepageTemplate/HomepageTemplate";
 
 //TODO mock data
 
 const heroes = [
   {
     image: "https://via.placeholder.com/150",
-    title: "Hero 1",
-    subtitle: "Subtitle 1",
+    name: "Hero 1",
+    abilities: "Subtitle 1",
+    origin: "Subtitle 1",
   },
   {
     image: "https://via.placeholder.com/150",
-    title: "Hero 2",
-    subtitle: "Subtitle 2",
+    name: "Hero 2",
+    abilities: "Subtitle 2",
+    origin: "Subtitle 2",
   },
   {
     image: "https://via.placeholder.com/150",
-    title: "Hero 3",
-    subtitle: "Subtitle 3",
+    name: "Hero 3",
+    abilities: "Subtitle 3",
+    origin: "Subtitle 3",
+  },
+  {
+    image: "https://via.placeholder.com/150",
+    name: "Hero 3",
+    abilities: "Subtitle 3",
+    origin: "Subtitle 3",
+  },
+  {
+    image: "https://via.placeholder.com/150",
+    name: "Hero 3",
+    abilities: "Subtitle 3",
+    origin: "Subtitle 3",
   },
 ];
 
 export const Homepage = () => {
   return (
-    <div>
-      <HeroList heroes={heroes} />
-    </div>
+    <HomepageTemplate
+      header={<Header />}
+      heroList={<HeroList heroes={heroes} />}
+    />
   );
 };
