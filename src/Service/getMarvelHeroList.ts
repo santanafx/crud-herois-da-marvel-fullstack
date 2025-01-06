3126b14a5b8a91ae588fbfaca90c906e864536c3
lucas <santanafx@hotmail.com>
2025-01-06 14:53:30 -0300

    feat: added marvel API

---
+++ src/Service/getMarvelHeroList.ts
@@ -0,0 +1,19 @@
+export const getMarvelHeroList = async () => {
+  try {
+    const response = await fetch(
+      "http://gateway.marvel.com/v1/public/characters?ts=1&apikey=9f40709213062a89659b5a21fe06e494&hash=93bc794f20ec9af703d80fb042250d72",
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    const data = await response.json();
+    return { status: response.status, data };
+  } catch (error) {
+    console.error("Failed to fetch heroes list:", error);
+    throw error;
+  }
+};
