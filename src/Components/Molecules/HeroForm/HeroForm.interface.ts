28ff1c5ebfa226182cb10d6dc191cc42b5033cd9
lucas <santanafx@hotmail.com>
2025-01-06 14:52:30 -0300

    feat: new interface

--- src/Components/Molecules/HeroForm/HeroForm.interface.ts
+++ src/Components/Molecules/HeroForm/HeroForm.interface.ts
@@ -8,3 +8,68 @@ export interface HeroFormProps {
   };
   onClose: () => void;
 }
+
+interface Thumbnail {
+  path: string;
+  extension: string;
+}
+
+interface ComicItem {
+  resourceURI: string;
+  name: string;
+}
+
+interface SeriesItem {
+  resourceURI: string;
+  name: string;
+}
+
+interface StoryItem {
+  resourceURI: string;
+  name: string;
+  type: string;
+}
+
+interface EventItem {
+  resourceURI: string;
+  name: string;
+}
+
+interface UrlItem {
+  type: string;
+  url: string;
+}
+
+export interface MarvelHero {
+  id: number;
+  name: string;
+  description: string;
+  modified: string;
+  thumbnail: Thumbnail;
+  resourceURI: string;
+  comics: {
+    available: number;
+    collectionURI: string;
+    items: ComicItem[];
+    returned: number;
+  };
+  series: {
+    available: number;
+    collectionURI: string;
+    items: SeriesItem[];
+    returned: number;
+  };
+  stories: {
+    available: number;
+    collectionURI: string;
+    items: StoryItem[];
+    returned: number;
+  };
+  events: {
+    available: number;
+    collectionURI: string;
+    items: EventItem[];
+    returned: number;
+  };
+  urls: UrlItem[];
+}
