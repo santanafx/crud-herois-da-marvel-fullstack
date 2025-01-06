a73a06f4d49b82719a70bf0fa0af8668235b3540
lucas <santanafx@hotmail.com>
2025-01-06 02:08:57 -0300

    feat: added toastify container

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
+import { Bounce, ToastContainer } from "react-toastify";
 import { Homepage } from "./Pages/Homepage/Homepage";
 
 function App() {
-  return <Homepage />;
+  return (
+    <>
+      <Homepage />
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick={false}
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+        transition={Bounce}
+      />
+    </>
+  );
 }
 
 export default App;
