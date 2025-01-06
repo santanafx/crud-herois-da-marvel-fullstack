fe02a790bb2b99a8606b3b541c1311dcee6c0ebc
lucas <santanafx@hotmail.com>
2025-01-06 02:11:13 -0300

    refactor: removed mocked data and added backend integration

--- src/Pages/Homepage/Homepage.tsx
+++ src/Pages/Homepage/Homepage.tsx
@@ -1,47 +1,27 @@
+import useSWR from "swr";
 import { Header } from "../../Components/Organisms/Header/Header";
 import { HeroList } from "../../Components/Organisms/HeroList/HeroList";
 import { HomepageTemplate } from "../../Components/Template/HomepageTemplate/HomepageTemplate";
-
-//TODO remove mock data
-
-const heroes = [
-  {
-    image: "https://via.placeholder.com/150",
-    name: "Hero 1",
-    abilities: "Subtitle 1",
-    origin: "Subtitle 1",
-  },
-  {
-    image: "https://via.placeholder.com/150",
-    name: "Hero 2",
-    abilities: "Subtitle 2",
-    origin: "Subtitle 2",
-  },
-  {
-    image: "https://via.placeholder.com/150",
-    name: "Hero 3",
-    abilities: "Subtitle 3",
-    origin: "Subtitle 3",
-  },
-  {
-    image: "https://via.placeholder.com/150",
-    name: "Hero 3",
-    abilities: "Subtitle 3",
-    origin: "Subtitle 3",
-  },
-  {
-    image: "https://via.placeholder.com/150",
-    name: "Hero 3",
-    abilities: "Subtitle 3",
-    origin: "Subtitle 3",
-  },
-];
+import { getHeroesList } from "../../Service/getHeroesList";
 
 export const Homepage = () => {
+  const { isLoading, error, data: heroes } = useSWR("/heroes", getHeroesList);
+
+  if (error) {
+    <div>
+      <h1>Falha ao obter dados!</h1>
+    </div>;
+  }
+
   return (
     <HomepageTemplate
       header={<Header />}
-      heroList={<HeroList heroes={heroes} isLoading={false} />}
+      heroList={
+        <HeroList
+          heroes={heroes || { status: 0, data: [] }}
+          isLoading={isLoading}
+        />
+      }
     />
   );
 };
