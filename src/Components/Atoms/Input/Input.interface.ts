e122d5c6746f7b021f8967f020763e6c481d2e6a
lucas <santanafx@hotmail.com>
2025-01-06 15:07:35 -0300

    refactor: moved to diferent folder

--- src/Components/Atoms/Input/Input.interface.ts
+++
@@ -1,8 +0,0 @@
-import { Control } from "react-hook-form";
-
-export interface InputProps
-  extends React.InputHTMLAttributes<HTMLInputElement> {
-  control: Control<any>;
-  name: string;
-  label?: string;
-}
